0161945c791b280e70a1cc644003e8f486e5a45a
prashantpatel-807 <122272169+prashantpatel-807@users.noreply.github.com>
2024-11-21 10:33:47 +0530

    First: React-Side-Bar
    Basic React Side Bar
    -------------------------
    Use Mini variant drawer and minor changes.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Sidnave from './Sidenav/Sidenav';
+
+function App() {
+  return (
+    <>
+    <Sidnave />
+    </>
+  );
+}
+
+export default App;
